0c29df2def22dbb5a6617e3efd4254f919199ccc
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2023-07-05 12:34:02 +0300

    Created models tags and tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,15 @@
+from enum import unique
+
 from django.db import models
 
-# Create your models here.
+
+class Tags(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+
+
+class Tasks(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
+    marks = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tags)
